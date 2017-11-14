8e7f7090016d24ba2a04ce5aa587f614e7db8a79
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-11-14 18:18:49 -0500

    WeatherApp8b

---
+++ weatherApp8b/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+// using a function contructor form to create an object
+function MyWeather()
+{
+	var weatherWidget = new WeatherWidget($("#weather-widget")),
+	version = "8.1";
+
+	// creating a private function
+	function setStatus()
+	{
+		$("#weather-widget").fadeIn();
+		weatherWidget.update();
+	}
+
+	// creating a public function
+	this.start = function()
+	{
+		$("#app>header").append(version);
+		$("#getWeather").click(setStatus);
+	};
+} // end MyApp
+
+/* 	JQuery's shorthand for the document ready event handler
+		could be written: $(document).ready(handler);
+
+		When this page loads, we'll create a global variable
+		named "app" by attaching it to the "window" object
+		(part of the BOM - Browser Object Model)
+*/
+$(function() {
+	window.app = new MyWeather();
+	window.app.start();
+});
