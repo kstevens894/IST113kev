94ec44752797de44cb58010143df3beae0212a9d
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-11-21 15:45:10 -0500

    Page 210

--- weatherApp8b/weatherWidget.js
+++ weatherApp8b/weatherWidget.js
@@ -9,29 +9,28 @@ function WeatherWidget($widget)
 	
 	function getWeatherReport() 
 	{
-		$.get("data/weather.xml")
-			.done(function(data) {
-				populateWeather(data);
-			})
-			 .fail(function(jqXHR, textStatus, errorThrown) {
-				 showError(errorThrown);
+		$.get("data/weather.json", {
+			t: new Date().getTime()
+		})
+		.done(function(data) { populateWeather(data); })
+		.fail(function(jqXHR, textStatus, errorThrown) {
+			showError(errorThrown);
 			 });
 	}
 	
 	function populateWeather(data)
 	{
-		var $observation = $("current_observation", data);
+		var $observation = data.current_observation;
 		
-		$(".results header img", $widget)
-			.attr("src", $("icon_url", $observation).text());
-		$(".location>span", $widget)
-			.text($("location", data).text());
+		$(".results header img", $widget).attr("src",
+				observation.icon_url);
+		$(".location>span", $widget).text(data.location.city);
 			
 		$(".conditions>span").each(function(i, e)
 		{
 			var $span = $(this);
 			var field = $span.data("field");
-			$(this).text($(field, $observation).text());
+			$(this).text(observation["field"];
 		});
 		
 		$(".loading", $widget).fadeOut(function ()
