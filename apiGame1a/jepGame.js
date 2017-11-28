cc18663adb8f833996e8b539a6de846d54f137a6
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-11-28 14:55:20 -0500

    1a before

--- apiGame1a/jepGame.js
+++ apiGame1a/jepGame.js
@@ -19,7 +19,7 @@ $(function playJeopardy(){
 			url: 'http://jservice.io/api/random',
 			success: function(data){
 			$.each(data, function(i, quest) {
-				$jepq.after('<h1> '+ quest.question +' </h1>');
+				$jepq.before('<h1> '+ quest.question +' </h1>');
 			});
 			},
 			error: function() {
