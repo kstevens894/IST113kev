070b88c1dcb86a720c9746107be1a841df08b4e9
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-11-28 15:02:38 -0500

    1a append bind

--- apiGame1a/jepGame.js
+++ apiGame1a/jepGame.js
@@ -2,24 +2,24 @@ $(function playJeopardy(){
 
 		var $jepq = $('#jepq');
 	
-	/* $.ajax({
+	$.ajax({
 		url: 'http://jservice.io/api/random',
 		success: function(data){
 		 $.each(data, function(i, quest) {
-			 $jepq.hide('<h1> '+ quest.question +' </h1>');
+			 $jepq.append('<h1> '+ quest.question +' </h1>');
 		});
 	  },
 	  error: function() {
 		  alert('error loading quest');
 	  }
-	}); */
+	});
 	$('#next').on('click', function() {
 		
 		$.ajax({
 			url: 'http://jservice.io/api/random',
 			success: function(data){
 			$.each(data, function(i, quest) {
-				$jepq.append('<h1> '+ quest.question +' </h1>');
+				$jepq.bind('<h1> '+ quest.question +' </h1>');
 			});
 			},
 			error: function() {
