bde963a5a330b3a6d01c9ea03a1c2f025867d049
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-11-28 13:46:03 -0500

    1a update

--- apiGame1a/jepGame.js
+++ apiGame1a/jepGame.js
@@ -1,11 +1,12 @@
-$(function playJeopardy(){	
+$(function playJeopardy(){
+
+		var $jepq = $('#jepq');
 	$.ajax({
 		url: 'http://jservice.io/api/random'
 		success: function(data){
 		 $.each(data, function(i, quest) {
-			 
+			 $jepq.append('<li> question</li>');
 		});
-			
 	  }
 	});
 });
