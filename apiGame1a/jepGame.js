5883628d12be244ff64b8fe8cce9247eb22fd51f
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-11-28 15:46:56 -0500

    1a

--- apiGame1a/jepGame.js
+++ apiGame1a/jepGame.js
@@ -6,7 +6,7 @@ $(function playJeopardy(){
 		url: 'http://jservice.io/api/random',
 		success: function(data){
 		 $.each(data, function(i, quest) {
-			 $jepq.append('<h1> '+ quest.question +' </h1>');
+			 $jepq.toggle('<h1> '+ quest.question +' </h1>');
 		});
 	  },
 	  error: function() {
@@ -19,7 +19,7 @@ $(function playJeopardy(){
 			url: 'http://jservice.io/api/random',
 			success: function(data){
 			$.each(data, function(i, quest) {
-				$jepq.appendToggle('<h1> '+ quest.question +' </h1>');
+				$jepq.append('<h1> '+ quest.question +' </h1>');
 			});
 			},
 			error: function() {
