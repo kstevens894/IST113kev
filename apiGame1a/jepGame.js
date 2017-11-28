aa6904eb311187b70dfeba50da265d7c8ed03499
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-11-28 15:59:50 -0500

    1a update

--- apiGame1a/jepGame.js
+++ apiGame1a/jepGame.js
@@ -9,7 +9,11 @@ $(function playJeopardy(){
 			 $jepq.append('<h1> '+ quest.question +' </h1>');
 		});
 	  },
-	  $('#next').on('click', function() {
+	  error: function() {
+		  alert('error loading quest');
+	  }
+	});
+	$('#next').on('click', function() {
 		
 		$.ajax({
 			url: 'http://jservice.io/api/random',
@@ -23,19 +27,4 @@ $(function playJeopardy(){
 			}
 		});
 	}); 
-	});
-	/* $('#next').on('click', function() {
-		
-		$.ajax({
-			url: 'http://jservice.io/api/random',
-			success: function(data){
-			$.each(data, function(i, quest) {
-				$jepq.reset('<h1> '+ quest.question +' </h1>');
-			});
-			},
-			error: function() {
-				alert('error saving question');
-			}
-		});
-	});  */
 });
