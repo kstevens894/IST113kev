b3b746c458291d18e3002ea5b66452b35101af20
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-11-27 21:29:17 -0500

    8c update

--- weatherApp8c/weatherWidget.js
+++ weatherApp8c/weatherWidget.js
@@ -13,7 +13,7 @@ function WeatherWidget($widget, wuKey)
 		$.ajax({
 /* 			url: "https://api.weather.gov/points/43.22,-71.53" + wuKey +
 				 "/conditions/q/" + coords + ".json", */
-			url: "https://forecast-v3.weather.gov/point/",
+			url: "https://forecast-v3.weather.gov/point/" + coords,
 			dataType : "json"
 		})
 		.done(function(data) { populateWeather(data); })
