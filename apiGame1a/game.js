886566326c5831c2386743f92ede98217637fc8a
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2017-12-02 13:40:33 -0500

    api1

--- apiGame1a/game.js
+++ apiGame1a/game.js
@@ -2,14 +2,18 @@ function playJeopardy(){
 	//jepq= jeopardy header1 display question there
 	//next = button to net question to appear
 	var $jepq = $("#jepq");
+	var $answerhide = $("#answerhide");
 	//when button is clicked make ajax call to api/random
 	$('#next').on('click',function(){
 		$.ajax({
-			url: 'http://jservice.io/api/random',
+			url: 'http://jservice.io/api/clues?category=2',
 			dataType: 'json'
 	})
 	.done(function(data){
-		$jepq.text(data[0].question);
+		var nextQuestionID=Math.floor(Math.random() *45) ;
+		$jepq.text(data[nextQuestionID].question);
+		$answerhide.text(data[nextQuestionID].answer);
+		
 	})
 	.fail(function(jqXHR, textStatus, errorThrown){
 	});
@@ -19,5 +23,17 @@ function playJeopardy(){
 	$(function(){
 		playJeopardy();
 	});
-	
-	
+	/* $("#answer").keypress(function(e)
+	{
+		if (e.which == 13) //Entere keypress
+		{
+			if (answer == answerhide)
+			{
+				alert("Correct!!! 5 points have been added to your score")
+			}
+			else
+			{
+				alert("Sorry you are incorrect 1 point has been deducted from your score...")
+			}
+		}
+	}); */
